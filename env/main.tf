a92b54b0184188a5ab884371195515b171d3254e
Graham Whitehouse <83427700+gwairslip@users.noreply.github.com>
2021-12-22 21:14:46 +0000

    Add environment

---
+++ env/main.tf
@@ -0,0 +1,70 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.68.0"
+    }
+  }
+
+  backend "azurerm" {
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+locals {
+  tags = {
+    Environment = var.environment
+  }
+  app_id = "integration"
+  short_app_id = "int"
+  short_environment = var.short_environment
+  location = var.location
+  api_key = var.api_key
+  integrations_hostname = var.integrations_hostname
+}
+
+module "ingredient_bowl" {
+  source              = "./tf_modules/Airslip.Terraform.Modules/modules/core/resource_group"
+
+  tags                = local.tags
+  app_id              = local.app_id
+  short_environment   = local.short_environment
+  location            = local.location
+}
+
+module "func_app_host" {
+  source = "./tf_modules/Airslip.Terraform.Modules/recipes/function_app_multiple_apps"
+
+  app_configuration = {
+    app_id = "${local.app_id}-hub",
+    short_app_id = "${local.short_app_id}-hub",
+    short_environment = local.short_environment,
+    location = local.location,
+    tags = local.tags,
+    account_tier = "Standard",
+    account_replication_type = "LRS"
+  }
+
+  resource_group = {
+    use_existing = true,
+    resource_group_name = module.ingredient_bowl.name,
+    resource_group_location = module.ingredient_bowl.location
+  }
+
+  function_apps = [
+    {
+      function_app_name = "hub",
+      app_settings = {
+        "EnvironmentSettings:EnvironmentName": var.environment,
+        "PublicApiSettings:Settings:Base:BaseUri": "https://something",
+        "PublicApiSettings:Settings:Api2Cart:BaseUri": locals.integrations_hostname,
+        "PublicApiSettings:Settings:Api2Cart:ApiKey": locals.api_key,
+        "PublicApiSettings:Settings:Vend:BaseUri": locals.integrations_hostname,
+        "PublicApiSettings:Settings:Vend:ApiKey": locals.api_key
+      }
+    }
+  ]
+}
---
+++ env/variables/qa/backend.tfvars
@@ -0,0 +1,4 @@
+resource_group_name  = "airslip-states-qa"
+storage_account_name = "airslipqatfstates"
+container_name       = "core-apps"
+key                  = "customer-portal-qa.terraform.tfstate"
