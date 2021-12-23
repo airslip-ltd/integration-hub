7c78bb1684dfac3422ec318ce3a71a10ebcbfdf3
Tom McDonough <tmcdonough@airslip.com>
2021-12-23 14:10:33 +0000

    Changed base uri for service

--- env/main.tf
+++ env/main.tf
@@ -62,7 +62,7 @@ module "func_app_host" {
       function_app_name = "proc",
       app_settings = {
         "EnvironmentSettings:EnvironmentName": var.environment,
-        "PublicApiSettings:Settings:Base:BaseUri": "https://airslip-${local.short_environment}-integration-hub-proc-app.azurewebsites.net",
+        "PublicApiSettings:Settings:Base:BaseUri": local.integrations_hostname,
         "PublicApiSettings:Settings:Api2Cart:BaseUri": local.integrations_hostname,
         "PublicApiSettings:Settings:Api2Cart:ApiKey": local.api_key,
         "PublicApiSettings:Settings:Vend:BaseUri": local.integrations_hostname,
