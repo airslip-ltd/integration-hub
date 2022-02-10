ce33a5005c715b0bb5ae5e0179e3d8f650d69d6f
Graham Whitehouse <83427700+gwairslip@users.noreply.github.com>
2022-02-10 15:37:24 +0000

    Updated use of user_info

--- src/Airslip.IntegrationHub.Core/Models/Shopify/ShopifyAuthorisingDetail.cs
+++ src/Airslip.IntegrationHub.Core/Models/Shopify/ShopifyAuthorisingDetail.cs
@@ -8,8 +8,17 @@ public class ShopifyAuthorisingDetail : ShortLivedAuthorisationDetail
     public override string ShortLivedCode { get; set; } = string.Empty;
         
     [JsonProperty(PropertyName = "state")]
-    public override string EncryptedUserInfo { get; set; } = string.Empty;
-        
+    public string StateEncryptedUserInfo { get; set; } = string.Empty;
+    
+    [JsonProperty(PropertyName = "user_info")]
+    public string? PassedEncryptedUserInfo { get; set; }
+
+    public override string EncryptedUserInfo
+    {
+        get => PassedEncryptedUserInfo ?? StateEncryptedUserInfo;
+        set => StateEncryptedUserInfo = value;
+    }
+
     [JsonProperty(PropertyName = "shop")]
     public override string StoreName { get; set; } = string.Empty;
         
