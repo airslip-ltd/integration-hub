03eb2a03dfc3215cceab0531b285d4ebb7dcffd1
Tom McDonough <tmcdonough@airslip.com>
2022-03-03 16:09:12 +0000

    Added hmac validation request types

--- src/Airslip.IntegrationHub.Core/Interfaces/IRequestValidationService.cs
+++ src/Airslip.IntegrationHub.Core/Interfaces/IRequestValidationService.cs
@@ -1,8 +1,12 @@
+using Airslip.IntegrationHub.Core.Models;
 using Microsoft.Azure.Functions.Worker.Http;
 
 namespace Airslip.IntegrationHub.Core.Interfaces;
 
 public interface IRequestValidationService
 {
-    bool ValidateRequest(ProviderDetails providerDetails, HttpRequestData req);
+    bool ValidateRequest(
+        ProviderDetails providerDetails,
+        HttpRequestData req,
+        AuthRequestTypes authRequestType);
 }
