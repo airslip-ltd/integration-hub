b2f8cdb6c8c53a4786f6c6c2d747fdb75ac9b443
Tom McDonough <tmcdonough@airslip.com>
2022-03-31 11:56:19 +0100

    Changed oauth URL / integrations

--- src/Airslip.IntegrationHub.Services.UnitTests/CallbackServiceTests.cs
+++ src/Airslip.IntegrationHub.Services.UnitTests/CallbackServiceTests.cs
@@ -37,21 +37,21 @@ namespace Airslip.IntegrationHub.Services.UnitTests
         // [Theory]
         // [InlineData(
         //     "_3dcart",
-        //     "?client_id=app-id&redirect_uri=https://dev-integrations.airslip.com/oauth/v1/auth/callback/_3DCart&response_type=code&shop=airslip-development",
+        //     "?client_id=app-id&redirect_uri=https://dev-oauth.airslip.com/oauth/v1/auth/callback/_3DCart&response_type=code&shop=airslip-development",
         //     "state",
         //     "https://apirest.3dcart.com/oauth/authorize?client_id=app-id&redirect_uri=callback-uri&response_type=code&store_url=https://airslip-development.3dcartstores.com")]
          // [InlineData("shopify",
          //     "?shop=airslip-development.myshopify.com&isOnline=true",
          //     "state",
-         //     "https://airslip-development.myshopify.com/admin/oauth/authorize?client_id=client-id&scope=read_orders,read_products,read_inventory&redirect_uri=https://dev-integrations.airslip.com/oauth/v1/auth/callback/Shopify&grant_options[]=per-user")]
+         //     "https://airslip-development.myshopify.com/admin/oauth/authorize?client_id=client-id&scope=read_orders,read_products,read_inventory&redirect_uri=https://dev-oauth.airslip.com/oauth/v1/auth/callback/Shopify&grant_options[]=per-user")]
          // [InlineData("vend",
          //     "",
          //     "state",
-         //     "https://secure.vendhq.com/connect?response_type=code&client_id=SrSLyYuwnffktH2oGJEJbQTiCXzkHgoL&redirect_uri=https://dev-integrations.airslip.com/oauth/v1/auth/callback/Vend")]
+         //     "https://secure.vendhq.com/connect?response_type=code&client_id=SrSLyYuwnffktH2oGJEJbQTiCXzkHgoL&redirect_uri=https://dev-oauth.airslip.com/oauth/v1/auth/callback/Vend")]
          // [InlineData("squarespace",
          //     "?shop=airslip-development",
          //     "state",
-         //     "https://airslip-development.squarespace.com/api/1/login/oauth/provider/authorize?client_id=client-id&scope=website.orders.read,website.transactions.read,website.inventory.read,website.products.read&redirect_uri=https://dev-integrations.airslip.com/oauth/v1/auth/callback/Squarespace&access_type=offline")]
+         //     "https://airslip-development.squarespace.com/api/1/login/oauth/provider/authorize?client_id=client-id&scope=website.orders.read,website.transactions.read,website.inventory.read,website.products.read&redirect_uri=https://dev-oauth.airslip.com/oauth/v1/auth/callback/Squarespace&access_type=offline")]
          // public void Can_generate_callback_url(string provider, string queryString, string relayQueryString, string expectedResult)
          // {
          //     PosProviders posProvider = provider.ParseIgnoreCase<PosProviders>();
@@ -109,12 +109,12 @@ namespace Airslip.IntegrationHub.Services.UnitTests
         //  [Theory]
         //  [InlineData("Shopify",
         //      "?code=9eb34b1a83917cee25bb0199c8711bab&hmac=24bd4063d10d51f5f117f8f9e936412cbc71a049400d3dd58b0407c8737b1bf3&host=YWlyc2xpcC1kZXZlbG9wbWVudC5teXNob3BpZnkuY29tL2FkbWlu&shop=airslip-development.myshopify.com&state=b951eMbRF6NelKyGXt8cRaj%2Fflv3G2GKHQ3N0vhPQhscLKW2bk6JoOc5rS4EzFP7MV%2F5ugljPQikkfowmDsZpomRpwieoZ41TMIgMu2H0nGx77YHnhearD2hFNkOvGd1&timestamp=1639833474",
-        //      "https://dev-integrations.airslip.com/api2cart/v1",
+        //      "https://dev-oauth.airslip.com/api2cart/v1",
         //      "https://airslip-development.myshopify.com/admin/oauth/access_token",
         //      "https://airslip-development.myshopify.com")]
         //  [InlineData("Squarespace",
         //      "?code=9eb34b1a83917cee25bb0199c8711bab&shop=airslip-development.squarespace.com&state=b951eMbRF6NelKyGXt8cRaj%2Fflv3G2GKHQ3N0vhPQhscLKW2bk6JoOc5rS4EzFP7MV%2F5ugljPQikkfowmDsZpomRpwieoZ41TMIgMu2H0nGx77YHnhearD2hFNkOvGd1",
-        //      "https://dev-integrations.airslip.com/api2cart/v1",
+        //      "https://dev-oauth.airslip.com/api2cart/v1",
         //      "https://login.squarespace.com/api/1/login/oauth/provider/tokens",
         //      "")]
         //  [InlineData("Cart3D")]
@@ -159,7 +159,7 @@ namespace Airslip.IntegrationHub.Services.UnitTests
         //      ProviderDetails providerDetails = _sut.GetProviderDetails(provider, queryString);
         //
         //      providerDetails.Provider.Should().Be(Enum.Parse<PosProviders>(provider));
-        //      providerDetails.DestinationBaseUri.Should().Be("https://dev-integrations.airslip.com/api2cart/v1");
+        //      providerDetails.DestinationBaseUri.Should().Be("https://dev-oauth.airslip.com/api2cart/v1");
         //      providerDetails.AuthorisingDetail.PermanentAccessUrl.Should().BeEmpty();
         //      providerDetails.AuthorisingDetail.BaseUri.Should().BeNull();
         //      providerDetails.AuthorisingDetail.EncryptedUserInfo.Should().BeEmpty();
