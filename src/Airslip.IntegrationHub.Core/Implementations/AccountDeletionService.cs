7ff69eec974d8958eb2f71b3e25f30c4fbe87e19
Tom McDonough <tmcdonough@airslip.com>
2022-04-09 14:28:41 +0100

    fixed code smells

--- src/Airslip.IntegrationHub.Core/Implementations/AccountDeletionService.cs
+++ src/Airslip.IntegrationHub.Core/Implementations/AccountDeletionService.cs
@@ -1,10 +1,8 @@
 using Airslip.Common.Deletion.Interfaces;
 using Airslip.Common.Deletion.Models;
-using Airslip.Common.Types.Failures;
 using Airslip.Common.Types.Interfaces;
 using Airslip.IntegrationHub.Core.Common.Discovery;
 using Airslip.IntegrationHub.Core.Interfaces;
-using Serilog;
 using System.Threading.Tasks;
 
 namespace Airslip.IntegrationHub.Core.Implementations;
@@ -13,20 +11,17 @@ public class AccountDeletionService : IDeletionService
 {
     private readonly IIntegrationDiscoveryService _discoveryService;
     private readonly IInternalMiddlewareClient _middlewareClient;
-    private readonly ILogger _logger;
 
-    public AccountDeletionService(
-        IIntegrationDiscoveryService discoveryService, IInternalMiddlewareClient middlewareClient, ILogger logger)
+    public AccountDeletionService(IIntegrationDiscoveryService discoveryService, IInternalMiddlewareClient middlewareClient)
     {
         _discoveryService = discoveryService;
         _middlewareClient = middlewareClient;
-        _logger = logger;
     }
 
-    public async Task<IResponse> DeleteRecord(string provider, string id, DeleteRequest requestDetails)
+    public async Task<IResponse> DeleteRecord(string integration, string id, DeleteRequest requestDetails)
     {
-        IntegrationDetails integrationDetails = _discoveryService.GetIntegrationDetails(provider);
+        IntegrationDetails integrationDetails = _discoveryService.GetIntegrationDetails(integration);
 
-        return await _middlewareClient.Delete(id,provider, integrationDetails, requestDetails);
+        return await _middlewareClient.Delete(id, integration, integrationDetails, requestDetails);
     }
 }
