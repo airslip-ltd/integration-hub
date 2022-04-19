03172a12eb5ef6a08aa23cc879f5dedd0d978a0f
Tom McDonough <tmcdonough@airslip.com>
2022-04-19 16:19:51 +0100

    Update variables.tf

--- env/variables.tf
+++ env/variables.tf
@@ -9,8 +9,6 @@ variable "environment" {
 }
 variable "api_key" {}
 variable "integrations_hostname" {}
-variable "yapilymatching_hostname" {}
-variable "mockdata_hostname" {}
 variable "ui_hostname" {}
 
 variable "admin_group_id" {}
