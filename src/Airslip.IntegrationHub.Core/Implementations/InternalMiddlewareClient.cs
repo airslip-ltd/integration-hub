6cce580935ded37e0c8594de9dde84982f58d23f
Tom McDonough <tmcdonough@airslip.com>
2022-04-27 17:57:30 +0100

    Added support to parse our own response to not lose conflict and notfound response types

--- src/Airslip.IntegrationHub.Core/Implementations/InternalMiddlewareClient.cs
+++ src/Airslip.IntegrationHub.Core/Implementations/InternalMiddlewareClient.cs
@@ -2,6 +2,7 @@
 using Airslip.Common.Types.Configuration;
 using Airslip.Common.Types.Enums;
 using Airslip.Common.Types.Failures;
+using Airslip.Common.Types.Hateoas;
 using Airslip.Common.Types.Interfaces;
 using Airslip.Common.Utilities;
 using Airslip.Common.Utilities.Extensions;
@@ -12,151 +13,163 @@ using Airslip.IntegrationHub.Core.Responses;
 using Microsoft.Extensions.Options;
 using Serilog;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Airslip.IntegrationHub.Core.Implementations
+namespace Airslip.IntegrationHub.Core.Implementations;
+
+public class InternalMiddlewareClient : IInternalMiddlewareClient
 {
-    public class InternalMiddlewareClient : IInternalMiddlewareClient
+    private readonly HttpClient _httpClient;
+    private readonly ILogger _logger;
+
+    public InternalMiddlewareClient(
+        HttpClient httpClient,
+        ILogger logger)
     {
-        private readonly HttpClient _httpClient;
-        private readonly ILogger _logger;
-        private readonly PublicApiSettings _publicApiSettings;
+        _httpClient = httpClient;
+        _logger = logger;
+    }
 
-        public InternalMiddlewareClient(
-            HttpClient httpClient,
-            ILogger logger,
-            IOptions<PublicApiSettings> options)
+    public async Task<IResponse> Authorise(string apiKey, string url)
+    {
+        try
         {
-            _httpClient = httpClient;
-            _logger = logger;
-            _publicApiSettings = options.Value;
+            _logger.Information("Posting to integration middleware for Url {PostUrl}", url);
+
+            HttpRequestMessage httpRequestMessage = new()
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri(url),
+                Headers =
+                {
+                    { "x-api-key", apiKey}
+                },
+                //Content = new StringContent(
+                //    Json.Serialize(middlewareAuthorisationRequest),
+                //    Encoding.UTF8,
+                //    Json.MediaType)
+            };
+
+            HttpResponseMessage response = await _httpClient.SendAsync(httpRequestMessage);
+                
+            string content = await response.Content.ReadAsStringAsync();
+
+            if (response.StatusCode == HttpStatusCode.BadRequest)
+            {
+
+                _logger.Error(
+                    "Error posting request to provider for Url {PostUrl}, response code: {StatusCode}, content: {Content}",
+                    url,
+                    response.StatusCode,
+                    content);
+
+                return new ErrorResponse("MIDDLEWARE_ERROR", $"Error authorising in internal middleware {content}");
+            }
+
+            _logger.Information("Got response for post to integration middleware for Url {PostUrl}, response code: {StatusCode}", url, response.StatusCode);
+
+            if (response.IsSuccessStatusCode)
+                return Json.Deserialize<IntegrationResponse>(content);
+
+            // TODO: This needs updating in the common library to return common handler
+            ErrorResponse errorObject = Json.Deserialize<ErrorResponse>(content);
+            if(string.IsNullOrEmpty(errorObject.ErrorCode))
+                return Json.Deserialize<ErrorResponses>(content);
+
+            return errorObject.ErrorCode switch
+            {
+                "RESOURCE_EXISTS" => Json.Deserialize<ConflictResponse>(content),
+                "RESOURCE_NOT_FOUND" => Json.Deserialize<NotFoundResponse>(content),
+                _ => errorObject
+            };
         }
-
-        public async Task<IResponse> Authorise(string apiKey, string url)
+        catch (HttpRequestException hre)
         {
-            try
-            {
-                _logger.Information("Posting to integration middleware for Url {PostUrl}", url);
-
-                HttpRequestMessage httpRequestMessage = new()
-                {
-                    Method = HttpMethod.Post,
-                    RequestUri = new Uri(url),
-                    Headers =
-                    {
-                        { "x-api-key", apiKey}
-                    },
-                    //Content = new StringContent(
-                    //    Json.Serialize(middlewareAuthorisationRequest),
-                    //    Encoding.UTF8,
-                    //    Json.MediaType)
-                };
-
-                HttpResponseMessage response = await _httpClient.SendAsync(httpRequestMessage);
-
-                if (response.StatusCode == HttpStatusCode.BadRequest)
-                {
-                    string content = await response.Content.ReadAsStringAsync();
-
-                    _logger.Error(
-                        "Error posting request to provider for Url {PostUrl}, response code: {StatusCode}, content: {Content}",
-                        url,
-                        response.StatusCode,
-                        content);
-
-                    return new ErrorResponse("MIDDLEWARE_ERROR", $"Error authorising in internal middleware {content}");
-                }
-
-                _logger.Information("Got response for post to integration middleware for Url {PostUrl}, response code: {StatusCode}", url, response.StatusCode);
-
-                return await response.CommonResponseHandler<IntegrationResponse>();
-            }
-            catch (HttpRequestException hre)
-            {
-                _logger.Error(hre,
-                    "Error posting request to integration middleware for Url {PostUrl}, response code: {StatusCode}",
-                    url, hre.StatusCode);
-                return new InvalidResource(nameof(Authorise), hre.Message);
-            }
-            catch (Exception ee)
-            {
-                _logger.Fatal(ee, "Error posting request to integration middleware for Url {PostUrl}", url);
-                return new InvalidResource("UNHANDLED_ERROR", ee.Message);
-            }
+            _logger.Error(hre,
+                "Error posting request to integration middleware for Url {PostUrl}, response code: {StatusCode}",
+                url, hre.StatusCode);
+            return new InvalidResource(nameof(Authorise), hre.Message);
         }
-
-        public async Task<IResponse> Delete(string accountId, string provider, IntegrationDetails integrationDetails, DeleteRequest deleteRequest)
+        catch (Exception ee)
         {
-            string url = Endpoints.Delete(integrationDetails.IntegrationSetting.PublicApiSetting.ToBaseUri(), provider, accountId);
-
-            try
-            {
-                _logger.Information("Posting to integration middleware for Url {PostUrl}",
-                    url);
-
-                HttpRequestMessage httpRequestMessage = new()
-                {
-                    Method = HttpMethod.Delete,
-                    RequestUri = new Uri(url),
-                    Headers =
-                    {
-                        { "x-api-key", integrationDetails.IntegrationSetting.PublicApiSetting.ApiKey}
-                    },
-                    Content = new StringContent(
-                        Json.Serialize(deleteRequest),
-                        Encoding.UTF8,
-                        Json.MediaType)
-                };
-
-                HttpResponseMessage response = await _httpClient.SendAsync(httpRequestMessage);
-
-                string content = await response.Content.ReadAsStringAsync();
-
-                if (!response.IsSuccessStatusCode)
-                {
-                    _logger.Error(
-                        "Error sending request to provider for Url {DeleteUrl}, response code: {StatusCode}",
-                        url,
-                        response.StatusCode);
-
-                    try
-                    {
-                        return Json.Deserialize<ErrorResponse>(content);
-                    }
-                    catch (Exception)
-                    {
-                        return new ErrorResponse("MIDDLEWARE_ERROR", $"Error deleting {content}");
-                    }
-                }
-
-                _logger.Information("Got response for post to integration middleware for Url {PostUrl}, response code: {StatusCode}", url, response.StatusCode);
-
-                return Json.Deserialize<DeleteResponse>(content);
-            }
-            catch (HttpRequestException hre)
-            {
-                _logger.Error(hre,
-                    "Error posting request to integration middleware for Url {PostUrl}, response code: {StatusCode}",
-                    url, hre.StatusCode);
-                return new InvalidResource(nameof(Authorise), hre.Message);
-            }
-            catch (Exception ee)
-            {
-                _logger.Fatal(ee, "Error posting request to integration middleware for Url {PostUrl}", url);
-                return new InvalidResource("UNHANDLED_ERROR", ee.Message);
-            }
+            _logger.Fatal(ee, "Error posting request to integration middleware for Url {PostUrl}", url);
+            return new InvalidResource("UNHANDLED_ERROR", ee.Message);
         }
     }
 
-    internal static class Endpoints
+    public async Task<IResponse> Delete(string accountId, string provider, IntegrationDetails integrationDetails, DeleteRequest deleteRequest)
     {
-        public static string Authorise(string baseUri, string provider) =>
-            $"{baseUri}/auth/{provider}";
+        string url = Endpoints.Delete(integrationDetails.IntegrationSetting.PublicApiSetting.ToBaseUri(), provider, accountId);
 
-        public static string Delete(string baseUri, string provider, string accountId) =>
-            $"{baseUri}/delete/{provider}/{accountId}";
+        try
+        {
+            _logger.Information("Posting to integration middleware for Url {PostUrl}",
+                url);
+
+            HttpRequestMessage httpRequestMessage = new()
+            {
+                Method = HttpMethod.Delete,
+                RequestUri = new Uri(url),
+                Headers =
+                {
+                    { "x-api-key", integrationDetails.IntegrationSetting.PublicApiSetting.ApiKey}
+                },
+                Content = new StringContent(
+                    Json.Serialize(deleteRequest),
+                    Encoding.UTF8,
+                    Json.MediaType)
+            };
+
+            HttpResponseMessage response = await _httpClient.SendAsync(httpRequestMessage);
+
+            string content = await response.Content.ReadAsStringAsync();
+
+            if (!response.IsSuccessStatusCode)
+            {
+                _logger.Error(
+                    "Error sending request to provider for Url {DeleteUrl}, response code: {StatusCode}",
+                    url,
+                    response.StatusCode);
+
+                try
+                {
+                    return Json.Deserialize<ErrorResponse>(content);
+                }
+                catch (Exception)
+                {
+                    return new ErrorResponse("MIDDLEWARE_ERROR", $"Error deleting {content}");
+                }
+            }
+
+            _logger.Information("Got response for post to integration middleware for Url {PostUrl}, response code: {StatusCode}", url, response.StatusCode);
+
+            return Json.Deserialize<DeleteResponse>(content);
+        }
+        catch (HttpRequestException hre)
+        {
+            _logger.Error(hre,
+                "Error posting request to integration middleware for Url {PostUrl}, response code: {StatusCode}",
+                url, hre.StatusCode);
+            return new InvalidResource(nameof(Authorise), hre.Message);
+        }
+        catch (Exception ee)
+        {
+            _logger.Fatal(ee, "Error posting request to integration middleware for Url {PostUrl}", url);
+            return new InvalidResource("UNHANDLED_ERROR", ee.Message);
+        }
     }
+}
+
+internal static class Endpoints
+{
+    public static string Authorise(string baseUri, string provider) =>
+        $"{baseUri}/auth/{provider}";
+
+    public static string Delete(string baseUri, string provider, string accountId) =>
+        $"{baseUri}/delete/{provider}/{accountId}";
 }
